792e009d449e7ce6285ea41daa081d5b0583d70a
ibrahimFahadCoding <139313449+ibrahimFahadCoding@users.noreply.github.com>
2023-09-21 20:56:00 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,50 @@
+import socket
+import threading
+
+host = input("Enter Server IP: ")
+port = int(input("Enter Server Port: "))
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen(2)
+
+clients, nicknames = [], []
+
+def broadcast(message):
+	for client in clients:
+		client.send(message)
+
+def handle(client):
+	while True:
+		try:
+			message = client.recv(1024)
+			broadcast(message)
+		except:
+			index = clients.index(client)
+			clients.remove(client)
+			client.close()
+			nickname = nicknames[index]
+			broadcast('{} exited Chat Room!'.format(nickname).encode('ascii'))
+			nicknames.remove(nickname)
+			break
+
+def receive():
+	while True:
+		client, address = server.accept()
+		print("Connected with {}".format(str(address)))
+		client.send('NICK'.encode('ascii'))
+		nickname = client.recv(1024).decode('ascii')
+		nicknames.append(nickname)
+		clients.append(client)
+
+		print("Nickname is {}".format(nickname))
+		broadcast("{} joined!".format(nickname).encode('ascii'))
+		client.send('Connected to server!'.encode('ascii'))
+
+		# Start Handling Thread For Client
+		thread = threading.Thread(target=handle, args=(client,))
+		thread.start()
+
+
+receive()
